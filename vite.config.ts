5f83bf50d002bb6d1ca37c36aeb3114932acf6e9
ThangaPrakashravi <prakashravichandran003@gmail.com>
2025-07-16 20:07:09 +0530

    fix: update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',
   server: {
     host: "::",
-    port: 5173,
-    base: "/thangaprakash-portfolio/"
+    port: 8080,
   },
   plugins: [
     react(),
